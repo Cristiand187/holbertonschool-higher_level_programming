4b0780a2133c0242024638e7230696432f433fb2
cristiand187 <cristiand187@hotmail.com>
2020-05-05 23:48:59 +0000

    Add task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
-    return (len(sentence), sentence[:1])
+    if (len(sentence) > 0):
+        return (len(sentence), sentence[:1])
+    else:
+        return (len(sentence), None)
