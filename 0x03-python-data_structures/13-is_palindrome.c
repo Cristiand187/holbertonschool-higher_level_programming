aefcd4d5c488a922a7b9a768ec8a9e2328b35b84
cristiand187 <cristiand187@hotmail.com>
2020-05-06 04:19:27 +0000

    Add task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int is_palindrome(listint_t **head)
+{
+    listint_t *current, *ini, *end;
+    unsigned int n_nodes = 0, i = 0, index = 0;
+
+    if (*head == NULL || head == NULL)
+        return (0);
+
+    current = *head;
+
+    while (current != NULL)
+    {
+        n_nodes += 1;
+        current = current->next;
+    }
+
+    if (n_nodes % 2 != 0)
+        return (0);
+    
+    current = *head;
+    for (i = 0; i < n_nodes / 2; i++)
+    {
+        index = 0;
+        ini = *head;
+        while (i != index)
+        {
+            ini = ini->next;
+            index += 1;
+        }
+        index = 0, end = *head;
+        while (index != (n_nodes -1) - i)
+        {
+            end = end->next;
+            index += 1;
+        }
+        if (ini->n != end->n)
+            return  (0);
+
+    }
+
+    return (1);
+}
