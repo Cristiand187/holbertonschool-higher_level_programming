f755a55f050dc91030d5102225ec0aeb3dc06556
cristiand187 <cristiand187@hotmail.com>
2020-05-19 22:09:44 +0000

    Add Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" class Square """
+
+
+class Square:
+    """empty class Square that defines a square
+
+    Attributes:
+        size (str): Description of size square.
+
+    """
+
+    def __init__(self, size):
+        """empty class Square that defines a square
+
+        Attributes:
+        self (obj): object
+        size (str): Description of size square.
+
+        """
+        self.__size = size
