55fbb42cdcfafecea40e231f947847a64a0826e7
vagrant <vagrant@vagrant.vm>
2020-05-25 23:36:32 +0000

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
     @width.setter
     def width(self, valor):
 
-        if (type(valor) != int):
+        if type(valor) != int:
             raise TypeError("width must be an integer")
         elif (valor < 0):
             raise ValueError("width must be >= 0")
@@ -44,8 +44,8 @@ class Rectangle:
     @height.setter
     def height(self, valor):
 
-        if (type(valor) != int):
-            raise ValueError("height must be an integer")
+        if type(valor) != int:
+            raise TypeError("height must be an integer")
         elif (valor < 0):
             raise ValueError("height must be >= 0")
         else:
