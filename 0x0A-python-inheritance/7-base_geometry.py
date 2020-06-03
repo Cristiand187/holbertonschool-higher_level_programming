60faaaf60a558df3248a7e98ff94b5243e05f9c8
cristiand187 <cristiand187@hotmail.com>
2020-06-03 00:29:39 +0000

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry():
             TypeError: [description]
             ValueError: [description]
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError(name + " must be an integer")
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
