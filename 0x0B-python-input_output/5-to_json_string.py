a6f5387fe8cb53daa09416c780ee7d63df2d53cf
cristiand187 <cristiand187@hotmail.com>
2020-06-03 23:39:59 +0000

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -4,5 +4,12 @@ import json
 
 
 def to_json_string(my_obj):
+    """[summary]
 
+    Arguments:
+        my_obj {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
     return json.dumps(my_obj)
