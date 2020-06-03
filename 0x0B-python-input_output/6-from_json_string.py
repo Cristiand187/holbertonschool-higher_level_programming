d7217209b2304b86dd4bb108c7401e73414cb8f2
cristiand187 <cristiand187@hotmail.com>
2020-06-03 23:42:27 +0000

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-import json
 """Module Json"""
+import json
 
 
 def from_json_string(my_str):
+    """[summary]
+
+    Arguments:
+        my_str {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
     return json.loads(my_str)
