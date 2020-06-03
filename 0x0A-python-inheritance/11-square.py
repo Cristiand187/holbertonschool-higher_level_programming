d66310a7ab63b381fa677dfb6ba66ace84060819
cristiand187 <cristiand187@hotmail.com>
2020-06-03 00:46:33 +0000

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -72,8 +72,17 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """[summary]
 
+    Arguments:
+        Rectangle {[type]} -- [description]
+    """
     def __init__(self, size):
+        """[summary]
+
+        Arguments:
+            size {[type]} -- [description]
+        """
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
