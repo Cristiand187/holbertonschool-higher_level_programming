b7830b817c377b7688771dc9e94bd64ee9751975
cristiand187 <cristiand187@hotmail.com>
2020-06-03 00:54:42 +0000

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""[summary]"""
+
+
+def is_kind_of_class(obj, a_class):
+    """[summary]
+
+    Arguments:
+        obj {[type]} -- [description]
+        a_class {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+    return isinstance(obj, a_class)
