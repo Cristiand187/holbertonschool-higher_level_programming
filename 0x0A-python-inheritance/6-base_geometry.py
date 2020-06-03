e12fd1eb397cd42414d33c5519a22c8654d00a1b
cristiand187 <cristiand187@hotmail.com>
2020-06-03 00:17:55 +0000

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry
+    """
+    pass
+
+    def area(self):
+        """[summary]
+
+        Raises:
+            ValueError: [description]
+        """
+        raise Exception("area() is not implemented")
