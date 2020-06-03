1fc98c285da0f1bd7d438bd8d5d7021a3f094a97
cristiand187 <cristiand187@hotmail.com>
2020-06-03 01:22:42 +0000

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
+"""[summary]    """
 
+
+def inherits_from(obj, a_class):
+    """[summary]
+
+    Arguments:
+        obj {[type]} -- [description]
+        a_class {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
     return issubclass(type(obj), a_class) and (type(obj) is not a_class)
