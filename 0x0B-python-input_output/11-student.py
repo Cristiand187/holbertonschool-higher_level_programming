87b918528872ba54231a7b473360da5ac78ad610
cristiand187 <cristiand187@hotmail.com>
2020-06-03 23:50:34 +0000

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student():
+    """[summary]
+    """
+    def __init__(self, first_name, last_name, age):
+        """[summary]
+
+        Arguments:
+            first_name {[type]} -- [description]
+            last_name {[type]} -- [description]
+            age {[type]} -- [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """[summary]
+
+        Returns:
+            [type] -- [description]
+        """
+        return self.__dict__
