bfd4110f08ea7473a04a4ce82733c7063937e568
vagrant <vagrant@vagrant.vm>
2020-06-05 21:15:24 +0000

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,7 +16,7 @@ def add_integer(a, b=98):
         ValueError: [b must be an integer]
     """
 
-    if (type(a) not in [float, int]):
+    if (type(a) not in [float, int] or a is None):
         raise TypeError("a must be an integer")
     elif (type(b) not in [float, int]):
         raise TypeError("b must be an integer")
