d53d592c7b35594b5bcd42a8138f2356d307c0e4
Cristiand187 <cristiand187@hotmail.com>
2020-07-15 12:19:55 +0000

    0x0E - Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,6 +2,6 @@
 -- list privileges for user_0d_1
 -- list privileges for user_0d_2
 SHOW GRANTS 
-    FOR user_0d_1@localhost;
+    FOR 'user_0d_1'@'localhost';
 SHOW GRANTS 
-    FOR user_0d_2@localhost;
+    FOR 'user_0d_2'@'localhost';
