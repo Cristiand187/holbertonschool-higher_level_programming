08dc1927e629184709532275b85b06bf02381411
Cristiand187 <cristiand187@hotmail.com>
2020-07-15 23:27:00 +0000

    0x0E - Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,9 @@
 CREATE DATABASE IF EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     state_id INT,
     name VARCHAR(256),
-    FOREIGN KEY(state_id) REFERENCES states(id));
+    FOREIGN KEY(state_id) 
+    REFERENCES states(id));
+
