eb75f1a97333c161a3c575d2cf539f6c36ddfb85
Cristiand187 <cristiand187@hotmail.com>
2020-07-16 04:22:14 +0000

    0x0E - Task 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,6 @@
 -- lists all shows from hbtn_0d_tvshows_rate
-SELECT tv_shows.title, SUM(rate) as rating
+-- SUM rate
+SELECT tv_shows.title, SUM(rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_shows.id
