07745e68be8c8a393a74022198a6c53779dcabc3
Cristiand187 <cristiand187@hotmail.com>
2020-07-16 02:52:14 +0000

    0x0E - Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.show_id
-GROUP BY tv_shows.title, tv_genres.name
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
