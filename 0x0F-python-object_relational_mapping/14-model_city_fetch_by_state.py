39db1d2ec2bbe76c6511fc90176fe5603e36d06a
Cristiand187 <Cristiand187@hotmail.com>
2020-08-09 16:41:19 -0500

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City, State)\
+                   .filter(City.state_id == State.id)\
+                   .order_by(City.id.asc()).all()
+
+    for cities, states in query:
+        print("{}: ({}) {}".format(states.name, cities.id, cities.name))
+    session.close()
