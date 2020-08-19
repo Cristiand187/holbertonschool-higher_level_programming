98e34199e10b7a37c965379a6240b5f8e668c5c4
Cristiand187 <Cristiand187@hotmail.com>
2020-08-18 23:26:04 -0500

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
     """ Python function to find peak number"""
-
-    if len(list_of_integers) > 0:
-        list_ = sorted(list_of_integers)
-        return list_[-1]
-    else:
-        return None
+    return sorted(list_of_integers)[-1] if len(list_of_integers) > 0 else None
