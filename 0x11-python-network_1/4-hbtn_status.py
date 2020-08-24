5dc52c576a3d9886a95dda26426edd64d8a8aa39
Cristiand187 <cristiand187@hotmail.com>
2020-08-24 17:26:35 -0500

    0x11. Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,10 @@
 """Write a Python script that fetches https://intranet.hbtn.io/status"""
 import requests
 
-url = 'https://intranet.hbtn.io/status'
-with requests.get(url) as response:
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with requests.get(url) as response:
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
