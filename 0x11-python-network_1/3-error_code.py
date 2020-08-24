f23a9362a54d5b693331e399dec2af1521c0c70d
Cristiand187 <cristiand187@hotmail.com>
2020-08-24 17:15:45 -0500

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ script that takes in a URL, sends a request to the URL and
+ displays the body of the response (decoded in utf-8).
+"""
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as response:
+            the_page = response.read()
+            print(the_page.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: ', e.code)
+    except URLError as e:
+        print('Reason: ', e.reason)
