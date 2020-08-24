bad4a27f5da942ae1fc70c62b38b8157b39ecee3
Cristiand187 <cristiand187@hotmail.com>
2020-08-24 18:23:01 -0500

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL, sends a
+request to the URL and displays the body of the response."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    status = r.status_code
+    if status >= 400:
+        print("Error code: " + str(status))
+    else:
+        print(r.text)
