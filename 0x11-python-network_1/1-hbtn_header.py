2376b342ada95832ac2231a525ad952101a3b31b
Cristiand187 <cristiand187@hotmail.com>
2020-08-24 17:01:32 -0500

    0x11. Python - Network #1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the value of the X-Request-Id"""
-import urllib.request
+from urllib.request import Request, urlopen
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
+    req = Request(url)
+    with urlopen(req) as response:
         print(response.headers.get('X-Request-Id'))
