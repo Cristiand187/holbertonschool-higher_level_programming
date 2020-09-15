47eb9d245085ddafec87f4c8a267cbbff0e18fe3
Cristiand187 <cristiand187@hotmail.com>
2020-09-15 17:46:08 -0500

    0x12. Javascript - Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// You must create a constant variable called myVar with the value “Javascript is
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
