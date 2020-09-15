34c8d35ce0110e8c571600a9feeff7f4c0e04a47
Cristiand187 <cristiand187@hotmail.com>
2020-09-15 17:39:00 -0500

    0x12. Javascript - Task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // You must create a constant variable called myVar with the value “Javascript is
 
-let myVar = 'Javascript is amazing';
+const myVar = 'Javascript is amazing';
 console.log(myVar);
