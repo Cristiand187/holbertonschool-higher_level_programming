be8cc43bdcf3508755227830b5d041372c09b7cf
Cristiand187 <cristiand187@hotmail.com>
2020-09-15 18:38:15 -0500

    0x12. Javascript - Task 3

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 // script that prints the first argument passed to it:
-let noarg = 'undefined'
-if (process.argv[3]) {
-    console.log(process.argv[2] + 'is ' + process.argv[3]);
-} else if (process.argv[2]) {
-    console.log(process.argv[2] + 'is ' + noarg);
-} else {
-    console.log(noarg + 'is ' + noarg);
-}
+
+console.log(process.argv[2]+ ' is '+ process.argv[3]);
