c484c1647e4a67b4a1f581a23b666438c1068376
Cristiand187 <cristiand187@hotmail.com>
2020-09-15 18:41:49 -0500

    0x12. Javascript - Task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // script that prints the first argument passed to it:
 
-console.log(process.argv[2]+ ' is '+ process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
