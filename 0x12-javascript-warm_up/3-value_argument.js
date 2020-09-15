dfed3b048097239856db907c648bcbfb21b94959
Cristiand187 <cristiand187@hotmail.com>
2020-09-15 18:32:43 -0500

    0x12. Javascript - Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// script that prints the first argument passed to it:
+// script that prints the first argument passed to it
 
 if (process.argv[2]) {
     console.log(process.argv[2]);
