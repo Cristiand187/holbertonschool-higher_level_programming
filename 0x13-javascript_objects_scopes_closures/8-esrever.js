9158094a99d4f06a54ec95ec5058cdb0db3b92e1
Cristiand187 <cristiand187@hotmail.com>
2020-09-21 18:18:46 -0500

    0x13. Javascript - Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,9 +2,9 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  const new_list = [];
+  const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    new_list.push(list[i]);
+    newList.push(list[i]);
   }
-  return new_list;
+  return newList;
 };
