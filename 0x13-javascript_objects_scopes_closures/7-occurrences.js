1e07ff9c7767b6a74e41d17ae76434afea663fa7
Cristiand187 <cristiand187@hotmail.com>
2020-09-21 18:02:46 -0500

    0x13. Javascript - Task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// unction that returns the number of occurrences in a list:
+
+exports.nbOccurences = function (list, searchElement) {
+  let numElement = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      numElement += 1;
+    }
+  }
+  console.log(numElement);
+};
