11b8d3666d19dcc42c5bdbd506c8deafa550813d
Cristiand187 <cristiand187@hotmail.com>
2020-09-22 20:58:37 -0500

    0x14. Javascript - Web scraping - Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (err, response) => {
+request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filePath, response.body, 'utf-8');
+    fs.writeFile(filePath, body, 'utf-8');
   }
 });
